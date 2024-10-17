065d4259e2820274387fe5934a634466fa8b6f8a
Tatiana <tanyusha_tokor@mail.ru>
2024-10-17 12:17:38 +0200

    cor cache

--- diary/urls.py
+++ diary/urls.py
@@ -8,7 +8,7 @@ app_name = DiaryConfig.name
 
 urlpatterns = [
     path('entries/', EntryListView.as_view(), name='entries'),
-    path('entry/<int:pk>', cache_page(60)(EntryDetailView.as_view()), name='entry'),
+    path('entry/<int:pk>', EntryDetailView.as_view(), name='entry'),
     path('create_entry/', EntryCreateView.as_view(), name='create_entry'),
     path('update_entry/<int:pk>', EntryUpdateView.as_view(), name='update_entry'),
     path('delete_entry/<int:pk>', EntryDeleteView.as_view(), name='delete_entry'),
